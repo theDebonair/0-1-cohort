0938fb23cdfbc1cca22e13571bc7811ff9061dac
Harshit Gupta <harshit4277448@live.com>
2024-01-15 08:44:03 +0530

    done middlewares

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -13,8 +13,24 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+  numberOfRequestsForUser = {};
+}, 1000);
+
+app.use((req, res, next) => {
+  const userId = req.headers["user-id"];
+
+  if (numberOfRequestsForUser[userId]) {
+    if (numberOfRequestsForUser[userId] > 5) {
+      res.status(404).send("Blocked!");
+    } else {
+      numberOfRequestsForUser[userId] += 1;
+      next();
+    }
+  } else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+});
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
